bb186185d374ff2d3e1946f4bbdcea67c97088c8
Bartosz Owczarek <bjowczarek@gmail.com>
2019-09-05 21:36:45 +0200

    permissions for s3lambda

--- amplify/backend/function/S3Triggerbe81f2e2/src/index.js
+++ amplify/backend/function/S3Triggerbe81f2e2/src/index.js
@@ -1,3 +1,11 @@
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var apiChangeAgentApiGraphQLAPIIdOutput = process.env.API_CHANGEAGENTAPI_GRAPHQLAPIIDOUTPUT
+var apiChangeAgentApiGraphQLAPIEndpointOutput = process.env.API_CHANGEAGENTAPI_GRAPHQLAPIENDPOINTOUTPUT
+
+Amplify Params - DO NOT EDIT */
 exports.handler = function(event, context) {
   //eslint-disable-line
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
